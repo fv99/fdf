2c662e2b1fc264df1e884114ccaab1d53b99e942
fvonsovs <filipvon@protonmail.com>
2023-03-08 14:44:07 +0100

    Added background rendering

--- fdf.c
+++ fdf.c
@@ -6,13 +6,13 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 16:29:59 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/03/07 17:05:30 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/03/08 14:43:54 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	put_pixel(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -20,19 +20,52 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+void	render_background(t_data *img, int color)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < WINDOW_HEIGHT)
+	{
+		y = 0;
+		while (y < WINDOW_WIDTH)
+			put_pixel(img, y++, x, color);
+		++x;
+	}
+}
+
+int	handle_close_keypress(int keysym, t_vars *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx, data->win);
+		data->win = NULL;
+	}
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Fuck you");
+	if (vars.win == NULL || vars.mlx == NULL)
+	{
+		free(vars.win);
+		return (1);
+	}
+	img.img = mlx_new_image(vars.mlx,  WINDOW_WIDTH, WINDOW_HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	render_background(&img, BACKGROUND_COLOUR);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, handle_close_keypress, &vars);
+	mlx_loop(vars.mlx);
+	mlx_destroy_image(vars.mlx, img.img);
+	mlx_destroy_display(vars.mlx);
+	free(vars.mlx);
 }
 
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:12:54 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/03/07 16:58:42 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/03/08 14:39:31 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,16 @@
 # include <math.h>
 # include <stdio.h>
 # include <unistd.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 1024
+
+// colours
+#define BACKGROUND_COLOUR 0x333333
+
+// structs
 typedef struct	s_data {
 	void	*img;
 	char	*addr;
@@ -28,6 +37,20 @@ typedef struct	s_data {
 	int		line_length;
 	int		endian;
 }	t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}	t_vars;
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
 
 
 #endif
